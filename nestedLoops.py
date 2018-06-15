c2a8c954dd9eed96aaf749f219e19ce7ee92225c
Peter Botros <peter.harby1@gmail.com>
2018-06-15 16:29:35 -0700

    Create nestedLoops.py

---
+++ nestedLoops.py
@@ -0,0 +1,7 @@
+# The Nested Loops
+# 24 clock demonstration
+
+for hour in range (24):
+    for minutes in range(60):
+        for second in range(60):
+            print(hour, ":", minutes, ":", second)
